41b1bab53d4d422bd0a135d8d1b8c912b95488a0
joramnv <joram.n.v@gmail.com>
2022-05-04 21:07:37 +0200

    Update dependency org.scalatest:scalatest to v3.2.12 (#18)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.7.0"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.8"
   lazy val doodle = "org.creativescala" %% "doodle" % "0.10.1"
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.11" % "test"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12" % "test"
   lazy val diffUtils = "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0" // needed for scalatest-snapshot-matcher-core dependency, included in /lib/scalatest-snapshot-matcher-core_3.jar
   // lazy val scalatestSnapshotMatcherCore = "com.commodityvectors" %% "scalatest-snapshot-matcher-core" % "2.0.4-SNAPSHOT" % "test" // included in /lib/scalatest-snapshot-matcher-core_3.jar
 }
